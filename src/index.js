9d15b466e165299a933dce8fcc71293a813b6a3c
AnkushJ <anqush.jamdagani@gmail.com>
2020-07-20 18:16:48 +0530

    created ball object with friction

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
+import Ball from "./Ball";
+
+const CANVAS_WIDTH = window.innerWidth;
+const CANVAS_HEIGHT = window.innerHeight;
+
+const INITIAL_SPEED = 5;
+const GRAVITY = 9.8;
+const FLUID_FRICTION = 0.001;
+const SURFACE_FRICTION = 0.01;
+
 const canvas = document.querySelector("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 
 const ctx = canvas.getContext("2d");
+
+const ball = new Ball(
+  {
+    x: Math.random() * CANVAS_WIDTH,
+    y: 0,
+    radius: 20,
+    fillColor: "crimson",
+    initialSpeed: INITIAL_SPEED,
+  },
+  {
+    ctx,
+    width: CANVAS_WIDTH,
+    height: CANVAS_HEIGHT,
+    gravity: GRAVITY,
+    fluidFriction: FLUID_FRICTION,
+    surfaceFriction: SURFACE_FRICTION,
+  }
+);
+
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ball.update();
+}
+
+animate();
