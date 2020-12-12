251ef41dbcea93a40a7634fbe2c0ce3b57eac894
AnkushJ <anqush.jamdagani@gmail.com>
2020-12-13 00:19:59 +0530

    build generated

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,57 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: { game: "./src/game/index.js", engine: "./src/engine/index.js" },
+  mode: 'development',
+  entry: { game: './src/game/index.js', engine: './src/engine/index.js' },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'doc'),
+    publicPath: '/'
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: "./dist",
+    contentBase: './doc'
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ['@babel/preset-env'],
             plugins: [
-              "@babel/plugin-proposal-class-properties",
-              "@babel/plugin-transform-runtime",
-            ],
-          },
-        },
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-transform-runtime'
+            ]
+          }
+        }
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
+        use: ['file-loader']
       },
       {
         test: /\.(mp3|wav)$/,
-        use: ["file-loader"],
+        use: ['file-loader']
       },
       {
         test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-        use: ["file-loader"],
-      },
-    ],
+        use: ['file-loader']
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new HtmlWebpackPlugin({
-      template: "src/game/index.html",
-    }),
-  ],
+      template: 'src/game/index.html'
+    })
+  ]
 };
