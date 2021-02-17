af8ec1064f186aeeb97909d22f7ab67eb04c8add
Ankush Jamdagani <anqush.jamdagani@gmail.com>
2021-02-17 05:42:26 +0530

    added time scale to debugger

--- src/editor/components/organisms/FPSDebugger/index.js
+++ src/editor/components/organisms/FPSDebugger/index.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Flex, Stat, StatLabel, StatNumber } from '@chakra-ui/react';
+import { Stat, StatLabel, StatNumber, StatGroup } from '@chakra-ui/react';
 
 import Panel from '../../molecules/Panel';
 
-const FPSDebugger = ({ fps }) => (
+const FPSDebugger = ({ fps, timeScale }) => (
   <Panel>
-    <Flex
+    <StatGroup
       p={4}
       bg='rgba(0, 0, 0, 0.1)'
       borderLeft='dashed 1px #ffffff38'
       borderBottom='dashed 1px #ffffff38'
-      width='80px'
+      flexShrink={0}
+      width='180px'
     >
       <Stat>
         <StatLabel fontSize='xs'>FPS</StatLabel>
         <StatNumber>{fps}</StatNumber>
       </Stat>
-    </Flex>
+      <Stat>
+        <StatLabel fontSize='xs'>Time Scale</StatLabel>
+        <StatNumber>{timeScale}</StatNumber>
+      </Stat>
+    </StatGroup>
   </Panel>
 );
 
-export default connect((state) => ({ fps: state.timing.fps }))(FPSDebugger);
+export default connect((state) => ({
+  fps: state.timing.fps,
+  timeScale: state.timing.timeScale
+}))(FPSDebugger);
